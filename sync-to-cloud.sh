7d17c06b28f4ca4acada810c2f29325e063687aa
Yunhong Yang <yy693@georgetown.edu>
2023-05-07 00:06:48 -0400

    change

--- sync-to-cloud.sh
+++ sync-to-cloud.sh
@@ -1,7 +1,7 @@
 
 
 # SYNC LOCAL VERSION OF WEBSITE TO GU-DOMAINS SERVER
-#rsync -alvr --delete Mini-Data-Viz-Project yunhong1@gtown.reclaimhosting.com:/home/yunhong1/public_html/
+rsync -alvr --delete Mini-Data-Viz-Project yunhong1@gtown.reclaimhosting.com:/home/yunhong1/public_html/
 
 # PUSH GIT REPO TO THE CLOUD FOR BACKUP
 DATE=$(date +"DATE-%Y-%m-%d-TIME-%H-%M-%S")
