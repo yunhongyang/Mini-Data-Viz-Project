13c4a4773b1e31f0a3feeaa684536012fe680cbf
Yunhong Yang <yy693@georgetown.edu>
2023-05-08 22:59:49 -0400

    GITHUB-UPLOAD:DATE-2023-05-08-TIME-22-59-49

--- sync-to-cloud.sh
+++ sync-to-cloud.sh
@@ -1,7 +1,7 @@
 
 
 # SYNC LOCAL VERSION OF WEBSITE TO GU-DOMAINS SERVER
-rsync -alvr --delete Code yunhong1@gtown.reclaimhosting.com:/home/yunhong1/public_html/
+rsync -alvr --delete 2023-04-14-job-search yunhong1@gtown.reclaimhosting.com:/home/yunhong1/public_html/
 
 # PUSH GIT REPO TO THE CLOUD FOR BACKUP
 DATE=$(date +"DATE-%Y-%m-%d-TIME-%H-%M-%S")
